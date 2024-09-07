ecacd15fc292dd381f29d8d2be502578dc581d10
Sterling-Gore <155609125+Sterling-Gore@users.noreply.github.com>
2024-09-07 00:38:06 -0500

    Copied over John's prototype map, added in movement with sprint and crouch speeds, added in mouse controls to turn the characters screen.

--- Call-From-Space/Assets/Scripts/CameraController.cs
+++ Call-From-Space/Assets/Scripts/CameraController.cs
@@ -5,17 +5,36 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    public float mouseSensitivity = 2f;
+    private float cameraVerticalRotation = 0f;
+    // (dont really use it) bool lockedCursor = true;
+    
 
     private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
        offset = transform.position - player.transform.position; 
+       Cursor.visible = false;
+       Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+
+        float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        cameraVerticalRotation -= inputY * mouseSensitivity * Time.deltaTime;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+
+        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+        
+        player.transform.Rotate(Vector3.up * inputX* mouseSensitivity * Time.deltaTime);
         transform.position = player.transform.position + offset;
     }
+
+    
 }
--- Call-From-Space/Assets/Scripts/PlayerController.cs
+++ Call-From-Space/Assets/Scripts/PlayerController.cs
@@ -4,26 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float standard_speed;
+    float speed;
     private Rigidbody rb;
+    public Transform camera;
 
     void Start()
     {
+        speed = standard_speed;
         rb = GetComponent<Rigidbody>();
     }
 
     void Movement()
     {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-        Vector3 Movement = new Vector3 (moveHorizontal, 0, moveVertical);
+        //inputs
+        float moveHorizontal = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+        float moveVertical = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+
+        //camera directions
+        Vector3 cameraForward = camera.forward;
+        Vector3 cameraRight = camera.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        //creating relative camera direction
+        Vector3 forwardRelative = moveVertical * cameraForward;
+        Vector3 rightRelative = moveHorizontal * cameraRight;
+
+        Vector3 movementDirection = forwardRelative + rightRelative;
+
+
+        // (OLD) Vector3 Movement = new Vector3 (moveHorizontal, 0, moveVertical);
+        Vector3 Movement = new Vector3 (movementDirection.x, 0, movementDirection.z);
     
-        transform.position += Movement * speed * Time.deltaTime;
+        transform.position += Vector3.ClampMagnitude(Movement, 1f);
     }
     
     
     void FixedUpdate()
-    {
+    {   
+        if (Input.GetKey("left shift"))
+        {
+            speed = standard_speed * 2f;
+        }
+        else if (Input.GetKey("c"))
+        {
+            speed = standard_speed * .5f;
+        }
+        else{
+            speed = standard_speed;
+        }
+        
         Movement();
     }
 }
