d0c86b36f8085213f3fc17ba5cf59303f6ccb96d
Sterling-Gore <155609125+Sterling-Gore@users.noreply.github.com>
2024-09-10 22:50:35 -0500

    worked on character speeds

--- Call-From-Space/Assets/PlayerController.cs
+++ Call-From-Space/Assets/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float jump_height;
     private Rigidbody rb;
 
     void Start()
@@ -17,13 +18,22 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
         Vector3 Movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
     
         transform.position += Movement * speed * Time.deltaTime;
+        
+        
     }
     
     
     void FixedUpdate()
     {
         Movement();
+
+        Vector3 jump = new Vector3 (0.0f, jump_height, 0.0f);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position += jump * Time.deltaTime;
+        }
     }
 }
