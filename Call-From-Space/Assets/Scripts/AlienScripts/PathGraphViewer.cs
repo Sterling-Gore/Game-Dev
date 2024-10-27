94f715a47519a00fda78f37035a4dedc6093e12d
Erregea5 <blastbaja15@gmail.com>
2024-10-26 22:23:45 -0500

    Integrated sterlings roam into my roam controller - made roam based on rooms rather than nodes - nodes now have radiuses where alien can roam around - alien chooses rooms closer to player when roaming more often

---
+++ Call-From-Space/Assets/Scripts/AlienScripts/PathGraphViewer.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+public class PathGraphViewer : MonoBehaviour
+{
+  List<Transform> nodes = new();
+  public bool draw = true;
+
+  void OnDrawGizmos()
+  {
+    if (!draw) return;
+    // nodes.Clear();
+    if (nodes.Count == 0)
+    {
+      foreach (Transform section in transform)
+        foreach (Transform room in section)
+          foreach (Transform node in room)
+            nodes.Add(node);
+    }
+    Gizmos.color = new Color(1, 0, 0, .5f);
+    nodes.ForEach(node =>
+    {
+      var pos = node.position;
+      pos.y = 2.658517f;
+      Gizmos.DrawSphere(pos, node.localScale.x);
+    });
+
+  }
+}
