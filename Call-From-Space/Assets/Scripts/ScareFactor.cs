774d6e4cdbbc4c0a4dcdf54f13fe7cec445d7c15
John Cox <john.cox0321@gmail.com>
2024-11-13 15:07:30 -0600

    add triggers around map

--- Call-From-Space/Assets/Scripts/ScareFactor.cs
+++ Call-From-Space/Assets/Scripts/ScareFactor.cs
@@ -5,86 +5,40 @@ using UnityEngine.UI;
 
 public class AudioTriggerImage : MonoBehaviour {
     public BoxCollider scareCollider;
-    public GameObject scareImage;
+    public GameObject scareImage; 
+    public RawImage[] scareImages; 
     public AudioSource scareAudio;
 
     private bool isFirstTime = true;
 
-    void Start () {
+    void Start() {
         if (scareImage != null) {
             scareImage.SetActive(false);
         }
     }
 
     void OnTriggerEnter(Collider other) {
-        if (isFirstTime && other.CompareTag("Player"))
-        {
-            isFirstTime = true;
-            StartCoroutine(ShowScareImage());
+        if (isFirstTime && other.CompareTag("Player")) {
+            isFirstTime = false; 
+            int randomIndex = Random.Range(0, scareImages.Length);
+            StartCoroutine(ShowScareImage(randomIndex));
         }
-    }   
-    private IEnumerator ShowScareImage()
-    {
-        // Display the scare image
-        if (scareImage != null)
-        {
+    }
+
+    private IEnumerator ShowScareImage(int randomIndex) {
+        if (scareImage != null && scareImages.Length > 0) {
+            Texture selectedTexture = scareImages[randomIndex].texture;
+            scareImage.GetComponent<RawImage>().texture = selectedTexture;
             scareImage.SetActive(true);
             scareAudio.Play();
         }
 
-        // Wait for 1 second
+        // Wait for 0.15 seconds
         yield return new WaitForSeconds(0.15f);
 
         // Hide the scare image
-        if (scareImage != null)
-        {
+        if (scareImage != null) {
             scareImage.SetActive(false);
         }
     }
-
-    // public AudioSource audioSource;  
-    // public AudioClip yesClip;        
-    // public RawImage displayImage;       
-
-    // private bool isImageDisplayed = false;
-
-    // void Start()
-    // {
-    //     // Ensure the image is initially hidden
-    //     if (displayImage != null)
-    //     {
-    //         displayImage.enabled = false;
-    //     }
-    // }
-
-    // void Update()
-    // {
-    //     // Check if the specified audio is playing
-    //     if (audioSource.isPlaying && audioSource.clip == yesClip && !isImageDisplayed)
-    //     {
-    //         StartCoroutine(ShowImageForOneSecond());
-    //     }
-    // }
-
-    // IEnumerator ShowImageForOneSecond()
-    // {
-    //     // Display the image
-    //     if (displayImage != null)
-    //     {
-    //         displayImage.enabled = true;
-    //     }
-
-    //     isImageDisplayed = true;
-
-    //     // Wait for 1 second
-    //     yield return new WaitForSeconds(1f);
-
-    //     // Hide the image
-    //     if (displayImage != null)
-    //     {
-    //         displayImage.enabled = false;
-    //     }
-
-    //     isImageDisplayed = false;
-    // }
 }
