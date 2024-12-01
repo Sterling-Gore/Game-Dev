cd887b18ea57869205349f0a489951e73bf99c21
Sterling-Gore <155609125+Sterling-Gore@users.noreply.github.com>
2024-11-30 19:35:51 -0600

    working on buttonMash script

--- Call-From-Space/Assets/Scripts/RoomX/ButtonMash.cs
+++ Call-From-Space/Assets/Scripts/RoomX/ButtonMash.cs
@@ -8,30 +8,43 @@ public class ButtonMash : Interactable
     //private KeyCode randomKey;
     private bool waitingForKey = false; 
 
+
+    bool PuzzleCompleted = false;
+    public GameObject PuzzleUI;
+    public GameObject player;
+
     public GameObject ButtonMashImage;
 
     public override string GetDescription()
     {
-        return ("Start Button Mash");
+        if(!PuzzleCompleted)
+            return ("Override Electrical Pannel");
+        else
+            return ("");
     }
 
     public override void Interact()
     {
-        Debug.Log("START");
-        StartCoroutine(StartButtonMash());
+        if(!PuzzleCompleted)
+            StartCoroutine(StartButtonMash());
     }
 
     IEnumerator StartButtonMash()
     {
-        while (true)
+        bool flag = true;
+        int count = 0;
+        while (flag)
         {
             // Generate a random key
             //randomKey = GetRandomKey();
             //Debug.Log("Press the key: " + randomKey);
 
+            yield return new WaitForSeconds(1f); // Short delay before next round
+
             waitingForKey = true;
             float timer = 0f;
             ButtonMashImage.SetActive(true);
+            ButtonMashImage.GetComponent<RectTransform>().anchoredPosition = new Vector2(Random.Range(-450,450), Random.Range(-200,150));
 
             // Wait for the correct key or timeout
             while (timer < timeLimit)
@@ -39,8 +52,8 @@ public class ButtonMash : Interactable
                 if (Input.GetKeyDown(KeyCode.V))
                 {
                     Debug.Log("Success! You pressed the correct key: ");
+                    count += 1;
                     waitingForKey = false;
-                    //yield return new WaitForSeconds(1f); // Short delay before next round
                     break;
                 }
 
@@ -51,11 +64,15 @@ public class ButtonMash : Interactable
             if (waitingForKey)
             {
                 Debug.Log("Time's up! You failed to press the correct key.");
+                count = 0;
                 waitingForKey = false;
             }
 
             ButtonMashImage.SetActive(false);
-            yield return new WaitForSeconds(1f); // Short delay before next round
+            if (count >= 4)
+            {
+                flag = false;
+            }
         }
     }
 }
